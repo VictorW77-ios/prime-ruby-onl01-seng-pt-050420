065bcc4e0fd55e15627bf1849ae18041bca7b4db
Victor Williams <victor.williams1030@gmail.com>
2020-05-13 21:17:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,6 @@ def prime?(number)
   return false if number.is_a?(1)
 
   (2..number-1).to_a.all? do |prime|
-    if number % prime != 0 
-      false
-    else 
-      true
-    end
+    number % prime != 0
   end
 end
